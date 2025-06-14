62701ab5240641a47a3402130a5f56666c402cf7
taisleal <tais.almeida.dev@gmail.com>
2025-06-14 12:27:30 -0300

    fixing carousel - language

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const carousel = document.getElementById('carousel');
 const totalSlides = carousel.children.length;
 let index = 0;
 
-function updateCarousel() {
+function updateSlide() {
   carousel.style.transform = `translateX(-${index * 100}vw)`;
 }
 
+function startAutoplay() {
+  setInterval(() => {
+    index = (index + 1) % totalSlides;
+    updateSlide();
+  }, 3000); 
+}
 
 function nextSlide() {
-  index = (index + 1) % totalSlides; 
-  updateCarousel();
+  index = (index + 1) % totalSlides;
+  updateSlide();
 }
 
 function prevSlide() {
-  index = (index - 1 + totalSlides) % totalSlides; 
-  updateCarousel();
+  index = (index - 1 + totalSlides) % totalSlides;
+  updateSlide();
 }
 
-
-setInterval(nextSlide, 4000); 
+window.addEventListener('load', startAutoplay);
